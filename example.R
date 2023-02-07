eb534d5a4a8598fb3b19fa2b0dc59f9c7ce0b1ba
BBErickson <bberickson@gmail.com>
2023-02-07 13:54:41 -0700

    example

--- example.R
+++ example.R
@@ -1,3 +1,27 @@
+# bind keyboard 
+
+library(shiny)
+library(keys)
+
+hotkeys <- c(
+  "1", 
+  "command+shift+k", 
+  "up up down down left right left right b a enter"
+)
+
+ui <- fluidPage(
+  useKeys(),
+  keysInput("keys", hotkeys)
+)
+
+server <- function(input, output, session) {
+  observeEvent(input$keys, {
+    print(input$keys)
+  })
+}
+
+shinyApp(ui, server)
+
 
 # remove search bar
 # Filter gene list show data table ----
